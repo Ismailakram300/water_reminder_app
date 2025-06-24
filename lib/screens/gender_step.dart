3dc86a07ded8c0c3d643bfb5299b9c992ed47577
Ismailakram300 <ismailakram300@gmail.com>
2025-06-24 11:47:50 +0500

    commit

--- lib/screens/gender_step.dart
+++ lib/screens/gender_step.dart
@@ -13,42 +13,66 @@ class GenderStep extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: ['Male', 'Female'].map((gender) {
-              bool isSelected = selectedGender == gender;
-              String imagePath = gender == 'Male'
-                  ? 'assets/images/male.png'
-                  : 'assets/images/female.png';
-              return GestureDetector(
-                onTap: () => onChanged(gender),
-                child: Column(
-                  children: [
-                  Image.asset(
-                  imagePath,
-                  height: 100,
-                  width: 100,
-                 ),
-                    Radio<String>(
-                      value: gender,
-                      groupValue: selectedGender,
-                      onChanged: (val) => onChanged(val!),
-                    ),
-                    Mytext(txt: gender),
-                  ],
-                ),
-              );
-            }).toList(),
+    return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 30),
+        Center(
+          child: Mytext(
+            decoration: TextDecoration.underline,
+            txt: "Choose your gender",
+            size: 26,
           ),
-        ],
-      ),
+        ),
+        const SizedBox(height: 130),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: ['Male', 'Female'].map((gender) {
+            bool isSelected = selectedGender == gender;
+            String imagePath = gender == 'Male'
+                ? 'assets/images/male.png'
+                : 'assets/images/female.png';
+
+            return GestureDetector(
+              onTap: () => onChanged(gender),
+              child: Column(
+                children: [
+                  Container(
+                    height: 130,
+                    width: 130,
+
+                    padding: const EdgeInsets.all(8),
+                    child: Image.asset(
+                      imagePath,
+
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Radio<String>(
+                        value: gender,
+                        groupValue: selectedGender,
+                        onChanged: (val) => onChanged(val!),
+                        activeColor: Colors.blue,
+                      ),
+                      Mytext(
+                        txt: gender,
+                        size: 19,
+                        color: isSelected
+                            ? Color(0xff278DE8)
+                            : Color(0xff979797),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+      ],
     );
+
   }
 }
