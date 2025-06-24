1ce282170d127cd0b51f6dd41ac378d13076be2e
Ismailakram300 <ismailakram300@gmail.com>
2025-06-24 11:47:55 +0500

    commit

--- lib/customs_widgets/mytext.dart
+++ lib/customs_widgets/mytext.dart
@@ -3,15 +3,24 @@ import 'package:flutter/cupertino.dart';
 class Mytext extends StatelessWidget {
   final String txt;
   final double size;
+  final TextDecoration decoration;
+  final Color color;
 
-  Mytext({required this.txt, this.size = 30, super.key});
+  Mytext({
+    required this.txt,
+    this.color =const Color(0xff030303),
+    this.size = 30,
+    this.decoration = TextDecoration.none,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       txt,
       style: TextStyle(
-        color: Color(0xff030303),
+        decoration: decoration,
+        color: color,
         fontFamily: 'Mulish',
         fontWeight: FontWeight.bold,
         fontSize: size,
