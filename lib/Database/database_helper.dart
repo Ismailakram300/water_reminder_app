e26a3fc1537c0ff939727b48c271d1f4ff4ead0a
Ismailakram300 <ismailakram300@gmail.com>
2025-06-30 10:51:04 +0500

    commit

--- lib/Database/database_helper.dart
+++ lib/Database/database_helper.dart
@@ -18,6 +18,24 @@ class DatabaseHelper {
     return _database!;
   }
 
+  // Future<Database> _initDB() async {
+  //   Directory docsDir = await getApplicationDocumentsDirectory();
+  //   String path = join(docsDir.path, 'user_data.db');
+  //
+  //   return await openDatabase(
+  //     path,
+  //     version: 2, // Increase version number
+  //     onCreate: _createDB,
+  //     onUpgrade: _onUpgrade, // Add this
+  //   );
+  // }
+  //
+  // Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+  //   if (oldVersion < 2) {
+  //     await db.execute('ALTER TABLE user_data ADD COLUMN selectedImage TEXT');
+  //   }
+  // }
+
   Future<Database> _initDB() async {
     Directory docsDir = await getApplicationDocumentsDirectory();
     String path = join(docsDir.path, 'user_data.db');
@@ -36,7 +54,11 @@ class DatabaseHelper {
         gender TEXT,
         weight INTEGER,
         wakeUpTime TEXT,
-        sleepTime TEXT
+        sleepTime TEXT,
+        dailyGoal INTEGER,
+        selectedImage TEXT,
+        selectedMl INTEGER
+        
       )
     ''');
   }
@@ -44,8 +66,11 @@ class DatabaseHelper {
   Future<void> saveUserData({
     required String gender,
     required int weight,
+    required int dailyGoal,
+    String? selectedImage,
     required String wakeUp,
     required String sleep,
+    int? selectedMl,
   }) async {
     final db = await database;
     await db.delete('user_data'); // Optional: only keep 1 record
@@ -54,6 +79,9 @@ class DatabaseHelper {
       'weight': weight,
       'wakeUpTime': wakeUp,
       'sleepTime': sleep,
+      'dailyGoal' :dailyGoal,
+      'selectedImage': selectedImage ?? '',
+      'selectedMl': selectedMl ?? 0,
     });
   }
 
