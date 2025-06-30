d1da33934f5adcd9ac1f06555eac471c8f3d6136
Ismailakram300 <ismailakram300@gmail.com>
2025-06-30 16:59:21 +0500

    Update settings.dart

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 import 'package:water_reminder_app/screens/reminder.dart';
+
+import '../Database/database_helper.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -56,6 +59,63 @@ class _SettingsScreenState extends State<SettingsScreen> {
   //     },
   //   );
   // }
+
+  void showDailyGoalDialog(BuildContext context, int initialValue) {
+    int currentValue = initialValue;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: Text("Daily Goal", style: TextStyle(fontWeight: FontWeight.bold)),
+          content: StatefulBuilder(
+            builder: (context, setState) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  NumberPicker(
+                    value: currentValue,
+                    minValue: 100,
+                    maxValue: 1000,
+                    step: 50,
+                    haptics: true,
+                    onChanged: (value) => setState(() => currentValue = value),
+                    selectedTextStyle: TextStyle(color: Colors.blue, fontSize: 28),
+                    textStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                  ),
+                  Text("ml", style: TextStyle(fontSize: 16, color: Colors.grey)),
+                ],
+              );
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("CANCEL"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await DatabaseHelper.instance.updateDailyGoal(currentValue);
+                await DatabaseHelper.instance.debugPrintAllUserData();
+                Navigator.of(context).pop();
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Daily goal set to $currentValue ml")),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+              ),
+              child: Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   String selectedSound = "Chime";
   void _showSoundPicker(BuildContext context) {
     List<String> sounds = ["Chime", "Bell", "Beep", "Drop"];
@@ -109,11 +169,11 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xffEFF7FF),
       appBar: AppBar(
         title: Text('Settings'),
         centerTitle: true,
-        backgroundColor: Colors.lightBlue[300],
+        backgroundColor: Colors.blue,
         elevation: 0,
       ),
       body: ListView(
@@ -131,7 +191,12 @@ class _SettingsScreenState extends State<SettingsScreen> {
               MaterialPageRoute(builder: (context) => ReminderScreen()),
             ),
           ),
-          _buildSettingsTile("Daily Goal"),
+          _buildSettingsTile("Daily Goal",  onTap: () async {
+            final userData = await DatabaseHelper.instance.getUserData();
+            final currentGoal = userData?['dailyGoal'] ?? 2000;
+
+            showDailyGoalDialog(context, currentGoal);
+          },),
 
           SizedBox(height: 20),
           _buildSectionTitle("Personal Information"),
@@ -157,7 +222,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         title,
         style: TextStyle(
           fontSize: 16,
-          color: Colors.blue,
+          color: Color(0xff278DE8),
           fontWeight: FontWeight.bold,
           decoration: TextDecoration.underline,
         ),
@@ -169,7 +234,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
     return Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: ListTile(
-        title: Text(label),
+        title: Text(
+          label,
+          style: TextStyle(color: Color(0xff727272), fontSize: 12),
+        ),
         trailing: Icon(Icons.arrow_forward_ios, size: 16),
         onTap: onTap,
       ),
