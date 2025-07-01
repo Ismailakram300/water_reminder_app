c479b28b112bf64cc653f645eb838f4c7cc8ea84
Ismailakram300 <ismailakram300@gmail.com>
2025-07-01 11:20:17 +0500

    commit

---
+++ lib/Models/drinks_log.dart
@@ -0,0 +1,11 @@
+class DrinkLog {
+  final int amount;
+  final DateTime timestamp;
+  final String imagePath;
+
+  DrinkLog({
+    required this.amount,
+    required this.timestamp,
+    required this.imagePath,
+  });
+}
