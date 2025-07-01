a6061e19760b224fb6580bb66077baa09b514c43
Ismailakram300 <ismailakram300@gmail.com>
2025-07-01 15:08:09 +0500

    commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +42,11 @@ android {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10") // or match your version
+
+    // ✅ Correct Kotlin syntax for desugaring lib:
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
