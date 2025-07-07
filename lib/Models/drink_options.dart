85c72b08f43bf83db4d3d7cb6b8fc16d8a38379c
Ismailakram300 <ismailakram300@gmail.com>
2025-07-07 10:51:35 +0500

    Commit

--- lib/Models/drink_options.dart
+++ lib/Models/drink_options.dart
@@ -12,12 +12,12 @@ class DrinkOption {
 
 
 List<DrinkOption> drinkOptions = [
-  DrinkOption(imagePath: 'assets/images/glass-water_37170541/glass-water.png', ml: 50),
-  DrinkOption(imagePath: 'assets/images/100ml.png', ml: 100),
-  DrinkOption(imagePath: 'assets/images/200ml.png', ml: 200),
-  DrinkOption(imagePath: 'assets/images/250ml.png', ml: 250),
-  DrinkOption(imagePath: 'assets/images/300ml.png', ml: 300),
+  DrinkOption(imagePath: 'assets/images/water_50ml/water_50ml', ml: 50),
+  DrinkOption(imagePath: 'assets/images/glass_100ml.png', ml: 100),
+  DrinkOption(imagePath: 'assets/images/glass_200ml.png', ml: 200),
+  DrinkOption(imagePath: 'assets/images/glass_250ml.png', ml: 250),
+  DrinkOption(imagePath: 'assets/images/glass_300ml.png', ml: 300),
   DrinkOption(imagePath: 'assets/images/350ml.png', ml: 350),
-  DrinkOption(imagePath: 'assets/images/400ml.png', ml: 400),
+  DrinkOption(imagePath: 'assets/images/glass_400ml.png', ml: 400),
   DrinkOption(imagePath: 'assets/images/450ml.png', ml: 450),
 ];
