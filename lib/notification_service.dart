1024467aebc5f71bf9c8fc1be9207ffeb56ca767
Ismailakram300 <123332697+Ismailakram300@users.noreply.github.com>
2025-07-21 10:39:03 +0500

    Update notification_service.dart

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -2,6 +2,8 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+
+
 class NotificationService {
   
   static Future<void> initialize() async {
